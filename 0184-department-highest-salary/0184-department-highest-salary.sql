cf7a2580daa45474855b6e20181715bba20a8c41
Ravichandran M <mravichandran760@gmail.com>
2025-10-23 20:36:14 +0530

    Time: 611 ms (75.44%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,11 +1,11 @@
-SELECT d.name AS Department,
-       e.name AS Employee,
-       e.salary AS Salary
-FROM Employee e
-JOIN (
-    SELECT departmentId, MAX(salary) AS max_salary
-    FROM Employee
-    GROUP BY departmentId
-) m ON e.departmentId = m.departmentId
-     AND e.salary = m.max_salary
-JOIN Department d ON e.departmentId = d.id;
+SELECT Department, Employee, Salary
+FROM (
+    SELECT d.name AS Department,
+           e.name AS Employee,
+           e.salary AS Salary,
+           DENSE_RANK() OVER (PARTITION BY e.departmentId ORDER BY e.salary DESC) AS rnk
+    FROM Employee e
+    JOIN Department d ON e.departmentId = d.id
+) t
+WHERE rnk = 1;
+
